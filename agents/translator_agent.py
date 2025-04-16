057f2d231f7e1cb6edb8c20e160e64a5f7adad57
Aaqil Ahamad <72489957+Aaqil456@users.noreply.github.com>
2025-04-16 17:47:55 +0800

    Update translator_agent.py

--- agents/translator_agent.py
+++ agents/translator_agent.py
@@ -22,11 +22,9 @@ class TranslatorAgent(BaseAgent):
         for article in articles:
             print(f"\n🔄 Translating article: {article.get('title')[:60]}...")
 
-
-            
             # --- Translate Title ---
             title_prompt = (
-                "Translate the following title into Malay (Bahasa Malaysia)  and make it SEO optimized title without changing the meaning.\n"
+                "Translate the following title into Malay (Bahasa Malaysia) and make it SEO optimized title without changing the meaning.\n"
                 "Only return the translated text without any explanation. Maintain crypto and trading topic related word in english in double quotes\n"
                 "Use natural, conversational, friendly Malaysian Malay — like how a friend shares info.\n"
                 "Keep it simple, relaxed, and easy to understand.\n"
@@ -63,19 +61,7 @@ class TranslatorAgent(BaseAgent):
             print("📝 Translated Title:", translated_title)
 
             # --- Translate Content ---
-            #content_prompt = (
-            #    "Translate the following tutorial content into Malay (Bahasa Malaysia). and make it SEO optimized article\n"
-            #   "Only return the translated text without any explanation. Maintain crypto and trading topic related word in english in double quotes\n"
-            #    "Use natural, conversational, friendly Malaysian Malay — like how a friend shares info.\n"
-            #    "Keep it simple, relaxed, and easy to understand.\n"
-            #    "Avoid exaggerated slang, emojis, or shouting.\n"
-            #    "Maintain HTML structure and <img> tags.\n"
-            #    "Do not translate anything inside <img> tags. \n\n"
-            #    f"{article['content']}"
-            #)
-
-            # --- Translate Content ---
-             content_prompt = (
+            content_prompt = (
                 "Translate and convert the following HTML tutorial article from English to Bahasa Malaysia in a blog post.\n\n"
                 "Your goal is to create a SEO-optimized, blog-style Bahasa Malaysia article suitable for Malaysian readers.\n\n"
                 "DO the following:\n"
@@ -101,9 +87,8 @@ class TranslatorAgent(BaseAgent):
                 "Use the following article as a style and tone reference. Match its voice, structure, and clarity:\n\n"
                 "Now translate and rewrite the following article:\n\n"
                 f"{article['content']}"
-            )   
+            )
 
-                        
             content_payload = {"contents": [{"parts": [{"text": content_prompt}]}]}
             translated_content = ""
 
