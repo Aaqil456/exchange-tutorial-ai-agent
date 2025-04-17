204353d0d77b8471cddfd82dcddf5b26293dff43
Aaqil Ahamad <72489957+Aaqil456@users.noreply.github.com>
2025-04-17 17:48:01 +0800

    Update scraper_agent.py

--- agents/scraper_agent.py
+++ agents/scraper_agent.py
@@ -14,7 +14,7 @@ class ScraperAgent(Agent):
         options_main.add_argument('--disable-dev-shm-usage')
         driver = uc.Chrome(options=options_main)
 
-        driver.get(f"{BASE_URL}/learn/trading-guide?page=8")
+        driver.get(f"{BASE_URL}/learn/trading-guide?page=9")
         soup = BeautifulSoup(driver.page_source, "html.parser")
         driver.quit()
 
